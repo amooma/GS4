7e125ba9dd0b5ae73076491803d16f460dba9b32
Philipp Kempgen <lists@kempgen.net>
2011-02-04 19:02:00 +0100

    '[db/schema.rb] Bitte löschen und "rake db:migrate" ausführen.'

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110203164006) do
-
-  create_table "extensions", :force => true do |t|
-    t.integer  "sip_account_id"
-    t.string   "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "phones", :force => true do |t|
-    t.string   "mac_address"
-    t.string   "ip_address"
-    t.integer  "provisioning_server_phone_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20110202104202) do
 
   create_table "sip_account_to_sip_users", :force => true do |t|
     t.integer  "sip_user_id"
     t.integer  "sip_account_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "sip_accounts", :force => true do |t|
-    t.integer  "phone_id"
-    t.string   "user"
-    t.string   "password"
-    t.string   "registrar"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
