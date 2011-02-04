4b18a41b93aeb7d020c01d3877759820d87c5d21
Philipp Kempgen <lists@kempgen.net>
2011-02-04 19:18:16 +0100

    Devise.setup: config.mailer_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,10 @@
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
+  
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me@config-initializers-devise.com"
+  config.mailer_sender = "pbx@localhost"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
