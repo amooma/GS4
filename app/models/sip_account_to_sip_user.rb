cbec960900784f7ab28cfba69f3e01be0146a2d6
Sascha Daniels <sd@alternative-solution.de>
2011-02-06 00:58:16 +0100

    More Validation

--- app/models/sip_account_to_sip_user.rb
+++ app/models/sip_account_to_sip_user.rb
@@ -1,2 +1,6 @@
 class SipAccountToSipUser < ActiveRecord::Base
+  validates_presence_of(:sip_user_id, :message => "sip_user_id needed")
+  validates_presence_of(:sip_account_id, :message => "sip_account_id needed")
+  validates_numericality_of(:sip_account_id, :message => "sip_account_id mus be a number")
+  validates_numericality_of(:sip_user_id, :message => "sip_user_id must be a number")
 end
--- app/models/user_to_phone.rb
+++ app/models/user_to_phone.rb
@@ -1,3 +1,7 @@
 class UserToPhone < ActiveRecord::Base
   has_many :users
+  validates_numericality_of(:user_id, :message => "User_id must be a number")
+  validates_numericality_of(:phone_id, :message => "phone_id must be a number")
+  validates_presence_of(:phone_id, :message => "phone_id needed")
+  validates_presence_of(:user_id, :message => "user_id needed")
 end
