7e9e086c52bf9f90442fd3365c64db65f8e0589c
Sascha Daniels <sd@alternative-solution.de>
2011-02-05 21:43:01 +0100

    Some Validations

--- app/models/sip_server.rb
+++ app/models/sip_server.rb
@@ -1,3 +1,6 @@
 class SipServer < ActiveRecord::Base
-   has_many :sip_users, :dependent => :destroy
+  has_many :sip_users, :dependent => :destroy
+  validates_presence_of(:name, :message => ":name missing")
+  validates_uniqueness_of(:name, :message => "Server allready exists")
+  validate_hostname_or_ip(:name)
 end
