dfc0a364df5c76a07597126d96b6c911a53b3e49
Sascha Daniels <sd@alternative-solution.de>
2011-02-07 23:09:07 +0100

    Validations

--- app/models/provisioning_server.rb
+++ app/models/provisioning_server.rb
@@ -1,2 +1,5 @@
 class ProvisioningServer < ActiveRecord::Base
+  validates_presence_of(:name, :message => ":name missing")
+  validates_uniqueness_of(:name, :message => "Server allready exists")
+  validate_hostname_or_ip(:name)
 end
