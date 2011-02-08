f7e361ddf76657ee60f9e69284272d45b00f5990
Philipp Kempgen <lists@kempgen.net>
2011-02-08 14:42:29 +0100

    whitespace gefixt

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,25 +1,27 @@
-class AuthenticationsController < ApplicationController  
-  def index  
-    @authentications = current_user.authentications if current_user  
-  end  
-  
-def create  
-  omniauth = request.env["omniauth.auth"]  
-  authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])  
-  if authentication  
-    flash[:notice] = "Signed in successfully."  
-    sign_in_and_redirect(:user, authentication.user)  
-  else  
-    current_user.authentications.create(:provider => omniauth['provider'], :uid => omniauth['uid'])
-    flash[:notice] = "Authentication successful."  
-    redirect_to authentications_url  
-  end  
-end   
-  
-  def destroy
-    @authentication = current_user.authentications.find(params[:id])  
-    @authentication.destroy  
-    flash[:notice] = "Successfully destroyed authentication."  
-    redirect_to authentications_url 
-  end  
-end  
+class AuthenticationsController < ApplicationController
+	
+	def index
+		@authentications = current_user.authentications if current_user
+	end
+	
+	def create
+		omniauth = request.env["omniauth.auth"]
+		authentication = Authentication.find_by_provider_and_uid( omniauth['provider'], omniauth['uid'] )
+		if authentication
+			flash[:notice] = "Signed in successfully."
+			sign_in_and_redirect(:user, authentication.user)
+		else
+			current_user.authentications.create( :provider => omniauth['provider'], :uid => omniauth['uid'] )
+			flash[:notice] = "Authentication successful."
+			redirect_to authentications_url
+		end
+	end
+	
+	def destroy
+		@authentication = current_user.authentications.find( params[:id] )
+		@authentication.destroy
+		flash[:notice] = "Successfully destroyed authentication."
+		redirect_to authentications_url
+	end
+	
+end
