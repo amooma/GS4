589fce82366495b5404bc0ea018e042bfb9463bf
Peter Kozak <spag@carcharoth.(none)>
2011-02-09 09:14:51 +0100

    Kamailio DBTEXT fixed for empty sip server entries

--- app/controllers/kamailio_controller.rb
+++ app/controllers/kamailio_controller.rb
@@ -1,6 +1,8 @@
 class KamailioController < ApplicationController
   def index
     @sip_users = SipUser.all
+    @sip_users.delete_if{|sip_user| sip_user.sip_server == nil}
+    
     respond_to do |format|
       format.txt      
     end
