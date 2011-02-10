afb6f006c1ccc1ceb5f227d6a66505989fbe07a9
Philipp Kempgen <lists@kempgen.net>
2011-02-10 23:46:57 +0100

    Reihenfolge der Validierungen

--- app/models/provisioning_server.rb
+++ app/models/provisioning_server.rb
@@ -11,9 +11,11 @@
 #
 
 class ProvisioningServer < ActiveRecord::Base
+  
   has_many :sip_phones
-  validates_presence_of(:name, :message => ":name missing")
-  validates_uniqueness_of(:name, :message => "Server already exists")
-  validate_hostname_or_ip(:name)
+  
+  validates_presence_of :name
+  validate_hostname_or_ip :name
+  validates_uniqueness_of :name, :message => "already exists"
   
 end
