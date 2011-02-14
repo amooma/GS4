581b3715f480776f8ac48ad7d6b13cddd2f8deb9
Sascha Daniels <sd@alternative-solution.de>
2011-02-14 14:47:44 +0100

    Mandatory setup steps added

--- app/controllers/pages_admin_controller.rb
+++ app/controllers/pages_admin_controller.rb
@@ -3,6 +3,11 @@ class PagesAdminController < ApplicationController
 	before_filter :authenticate_user!
 	
 	def index
+   if User.count == 0 || SipServer.count == 0 || ProvisioningServer.count == 0
+      respond_to do |format|
+      format.html { redirect_to(admin_setup_index_path) }
+      end
+  end 
 		begin
 			@number_of_users         = User               .count
 			@number_of_sip_accounts  = SipAccount         .count
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,11 @@ class PagesController < ApplicationController
 	
 	def index
 		begin
-			
+			 if User.count == 0 || SipServer.count == 0 || ProvisioningServer.count == 0
+      respond_to do |format|
+      format.html { redirect_to(admin_setup_index_path) }
+      end
+    end
 		#rescue
 		#	redirect_to('/db_migrate_missing')
 		end
