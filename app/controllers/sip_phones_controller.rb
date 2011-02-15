cf373089646442315e78c49900c50cef5e0ab9b7
Sascha Daniels <sd@alternative-solution.de>
2011-02-15 21:23:54 +0100

    Fixed update

--- app/controllers/sip_phones_controller.rb
+++ app/controllers/sip_phones_controller.rb
@@ -39,7 +39,7 @@ class SipPhonesController < ApplicationController
       @sip_phone.provisioning_server = ProvisioningServer.first
       setup_cantina_phone 
     end
-
+    
     respond_to do |format|
       if ProvisioningServer.count == 0
         format.html { redirect_to(new_provisioning_server_path, :alert => 'To create a new sip_phone you have to create a provisioning server first.') }
@@ -61,24 +61,25 @@ class SipPhonesController < ApplicationController
   # POST /sip_phones
   # POST /sip_phones.xml
   def create
-      @sip_phone = SipPhone.new(params[:sip_phone])
-      setup_cantina_phone 
-      
-      respond_to do |format|
-        if @sip_phone.save
-          format.html { redirect_to(@sip_phone, :notice => 'Sip phone was successfully created.') }
-          format.xml  { render :xml => @sip_phone, :status => :created, :location => @sip_phone }
-        else
-          format.html { render :action => "new" }
-          format.xml  { render :xml => @sip_phone.errors, :status => :unprocessable_entity }
-        end
+    @sip_phone = SipPhone.new(params[:sip_phone])
+    setup_cantina_phone 
+    
+    respond_to do |format|
+      if @sip_phone.save
+        format.html { redirect_to(@sip_phone, :notice => 'Sip phone was successfully created.') }
+        format.xml  { render :xml => @sip_phone, :status => :created, :location => @sip_phone }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @sip_phone.errors, :status => :unprocessable_entity }
       end
+    end
   end
   
   # PUT /sip_phones/1
   # PUT /sip_phones/1.xml
   def update
-    
+    @sip_phone = SipPhone.find(params[:id])
+    setup_cantina_phone
     respond_to do |format|
       if @sip_phone.update_attributes(params[:sip_phone])
         format.html { redirect_to(@sip_phone, :notice => 'Sip phone was successfully updated.') }
