af0a349afaf123dce2e092f0abf7028eb2ea4831
Sascha Daniels <sd@alternative-solution.de>
2011-02-21 10:44:36 +0100

    Catch missed rake db:migrate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,19 @@
 class PagesController < ApplicationController
+	rescue_from ActiveRecord::StatementInvalid, :with => :rescue_missing
 	
 	#before_filter :authenticate_user!
 	
 	def index
-		begin
-			if User.count == 0 || SipServer.count == 0 || ProvisioningServer.count == 0
-				respond_to do |format|
-					format.html { redirect_to(admin_setup_index_path) }
-				end
+		if User.count == 0 || SipServer.count == 0 || ProvisioningServer.count == 0
+			respond_to do |format|
+				format.html { redirect_to(admin_setup_index_path) }
 			end
-			rescue
-				redirect_to('/db_migrate_missing')
-		end
+		end	
 	end
 	
+private
+	def rescue_missing
+		redirect_to('/db_migrate_missing')
+	end
+
 end
