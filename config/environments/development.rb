bd4efb795e184fa84cc36ad2a02ffadef9b04702
Sascha Daniels <sd@alternative-solution.de>
2011-02-21 14:49:47 +0100

    Changed from Usermanager to Gemeinschaft4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Usermanager::Application.configure do
+Gemeinschaft4::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Usermanager::Application.config.secret_token = '00b94ce88ddac2e5173f942e0e0b186c30bac327b361058d23cb8aa2de75d97bae214f98edddcb71e8a4896433b4f4d0fffc3da734f677907534188c05f288f9'
+Gemeinschaft4::Application.config.secret_token = '00b94ce88ddac2e5173f942e0e0b186c30bac327b361058d23cb8aa2de75d97bae214f98edddcb71e8a4896433b4f4d0fffc3da734f677907534188c05f288f9'
