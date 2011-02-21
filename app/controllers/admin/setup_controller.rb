9db6e863a11870aaf89f51184d0de06bda8bea34
Sascha Daniels <sd@alternative-solution.de>
2011-02-21 10:44:09 +0100

    Fixed liks for moved admin page

--- app/controllers/admin/setup_controller.rb
+++ app/controllers/admin/setup_controller.rb
@@ -8,14 +8,14 @@ class Admin::SetupController < ApplicationController
       if User.count == 0      
         @user = User.new(params[:user])
         if @user.save
-          format.html { redirect_to(admin_path, :notice => "#{ @user.username } created. You can log in now.") }
-          format.xml  { render :xml => admin_path, :status => :created, :location => admin_user_path(@user) }
+          format.html { redirect_to(admin_index_path, :notice => "#{ @user.username } created. You can log in now.") }
+          format.xml  { render :xml => admin_index_path, :status => :created, :location => admin_user_path(@user) }
         else
           format.html { render :action => 'new' }
           format.xml  { render :xml => @user.errors, :status => :unprocessable_entity}
         end
       else
-        format.html { redirect_to(admin_path, :notice => "Setup disabled. Already done!") }
+        format.html { redirect_to(admin_index_path, :notice => "Setup disabled. Already done!") }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity}
       end
     end
