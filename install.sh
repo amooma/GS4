80144f282a657e315c966563378ecd89d7f4eda8
Sascha Daniels <sd@alternative-solution.de>
2011-02-22 06:50:44 -0800

    Dienste starten

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ make install
 
 cp /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.DIST
 cp /opt/Gemeinschaft4/misc/lighttpd.conf /etc/lighttpd/lighttpd.conf
+/etc/init.d/lighttpd restart
 cp -r /opt/Gemeinschaft4/misc/kamailio/etc/* /opt/kamailio-3.1/etc/kamailio/
 chgrp www-data /opt/kamailio-3.1/etc/kamailio/db_text/subscriber
 chgrp www-data /opt/kamailio-3.1/etc/kamailio/db_text/dbaliases
@@ -64,4 +65,4 @@ chmod g+rw /opt/kamailio-3.1/etc/kamailio/db_text/subscriber
 chmod g+rw /opt/kamailio-3.1/etc/kamailio/db_text/dbaliases
 cp /opt/Gemeinschaft4/misc/etc/init.d/kamailio /etc/init.d/
 update-rc.d  kamailio defaults
-
+/etc/init.d/kamailio start
