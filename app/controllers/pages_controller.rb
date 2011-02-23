bfadb16f127633971fc8d1f07edba25e394cd181
Sascha Daniels <sd@alternative-solution.de>
2011-02-23 17:43:30 +0100

    We only have an admin interface at the moment. If we have a user, we go to the admin interface.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,24 @@
 class PagesController < ApplicationController
-	rescue_from ActiveRecord::StatementInvalid, :with => :rescue_missing
-	
-	#before_filter :authenticate_user!
-	
-	def index
-		if User.count == 0 || SipServer.count == 0 || ProvisioningServer.count == 0 || SipProxy.count == 0
-			respond_to do |format|
-				format.html { redirect_to(admin_setup_index_path) }
-			end
-		end	
-	end
-	
-private
-	def rescue_missing
-		redirect_to('/db_migrate_missing')
-	end
-
+  rescue_from ActiveRecord::StatementInvalid, :with => :rescue_missing
+  
+  #before_filter :authenticate_user!
+  
+  def index
+    if User.count == 0 || SipServer.count == 0 || ProvisioningServer.count == 0 || SipProxy.count == 0
+      respond_to do |format|
+        format.html { redirect_to(admin_setup_index_path) }
+      end
+    end
+    if current_user
+      respond_to do |format|
+        format.html { redirect_to(admin_index_path) }
+      end
+    end
+  end
+  
+  private
+  def rescue_missing
+    redirect_to('/db_migrate_missing')
+  end
+  
 end
