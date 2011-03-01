5bd90183feda67410356ac4e166d8646f0e6fbb6
Philipp Kempgen <lists@kempgen.net>
2011-03-01 17:25:23 +0100

    SipServer: validates_numericality_of(:config_port, ...) gefixt

--- app/models/sip_proxy.rb
+++ app/models/sip_proxy.rb
@@ -15,8 +15,7 @@ class SipProxy < ActiveRecord::Base
   validates_presence_of(:name, :message => ":name missing")
   validates_uniqueness_of(:name, :message => "Server already exists")
   validate_hostname_or_ip(:name)
-  validates_numericality_of(:config_port,
-    :message => "Must be integer",
+  validates_numericality_of( :config_port,
     :allow_nil => true,
     :greater_than => 0,
     :less_than => 65536,
