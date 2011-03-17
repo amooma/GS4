43f074e82ca7150762bc555b820aabcd8b328b59
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-03-17 21:08:07 +0100

    messages der validations gefixt

--- app/models/sip_server.rb
+++ app/models/sip_server.rb
@@ -12,9 +12,10 @@
 
 class SipServer < ActiveRecord::Base
   has_many :sip_accounts, :dependent => :destroy
-  validates_presence_of(:name, :message => ":name missing")
-  validates_uniqueness_of(:name, :message => "Server already exists")
-  validate_hostname_or_ip(:name)
+  
+  validates_presence_of      :name
+  validates_uniqueness_of    :name
+  validate_hostname_or_ip    :name
   validates_numericality_of( :config_port,
     :allow_nil    => true,
     :greater_than =>     0,
