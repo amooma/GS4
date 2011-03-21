8c98f0c4c3f82499dcf99c8dc5a8266cf8970da9
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-03-21 16:39:36 +0100

    SipPhoneTest

--- test/unit/sip_phone_test.rb
+++ test/unit/sip_phone_test.rb
@@ -2,4 +2,17 @@ require 'test_helper'
 
 class SipPhoneTest < ActiveSupport::TestCase
   # testing sip_phone requires provisioning_server to be accessible
+  
+  should "not be valid without a phone_id if it has a provisioning_server_id" do
+    assert ! Factory.build( :sip_phone, :phone_id => nil ).valid?
+  end
+  
+  should "be valid with a phone_id if it has a provisioning_server_id" do
+    assert Factory.build( :sip_phone, :phone_id => 99999 ).valid?
+  end
+  
+  should "not be valid without a provisioning_server_id" do
+    assert ! SipPhone.new( :provisioning_server_id => nil, :phone_id => nil ).valid?
+  end
+  
 end
