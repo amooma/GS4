2994c66fc2782c9a2cd3458cd77d52527993e35b
Sascha Daniels <sd@alternative-solution.de>
2011-03-26 16:05:29 +0100

    Validation for managed_by_gs

--- app/models/sip_proxy.rb
+++ app/models/sip_proxy.rb
@@ -9,4 +9,14 @@ class SipProxy < ActiveRecord::Base
     :greater_than =>     0,
     :less_than    => 65536,
   )
+  
+  # TODO tests
+  validates_inclusion_of(:managed_by_gs, :in => [true, false], :message => "must be true or false!")
+  validates_length_of(:config_port, :maximum => 0, :message => "must not be set if not managed by GS!", :if => Proc.new { |sip_server| ! sip_server.managed_by_gs})
+  validates_presence_of(:config_port, :message => "needed if managed by GS!", :if => Proc.new { |sip_server| sip_server.managed_by_gs })
+  after_validation( :on => :update) do
+    if self.managed_by_gs != self.managed_by_gs_was
+      errors.add( :managed_by_gs, "can NOT be changed! Please delete server and create new one.")
+    end
+  end
 end
