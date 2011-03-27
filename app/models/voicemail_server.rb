a5b9c84991f8039d6a397733b4b4ff3d1c9263bc
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-03-27 17:13:30 +0200

    VoicemailServer

--- app/models/voicemail_server.rb
+++ app/models/voicemail_server.rb
@@ -3,21 +3,26 @@ class VoicemailServer < ActiveRecord::Base
   #has_many :sip_accounts
   
   validates_presence_of      :host
-  validates_uniqueness_of    :host
   validate_hostname_or_ip    :host
+  validates_uniqueness_of    :host
   validates_numericality_of( :port,
-    :allow_nil    => true,
+    :allow_nil    => true,              # empty SIP port means default SIP port (SRV lookups!)
     :greater_than =>     0,
     :less_than    => 65536,
   )
   
+  validate_hostname_or_ip(   :management_host, {
+    :message => "must be a valid hostname or IP address if set",
+    :allow_nil   => true,
+    :allow_blank => true,
+  })
   validates_uniqueness_of    :management_host
-  validate_hostname_or_ip    :management_host
   validates_numericality_of( :management_port,
     :allow_nil    => true,
     :greater_than =>     0,
     :less_than    => 65536,
   )
   
+  #TODO - Tests for the validations.
   
 end
--- test/unit/voicemail_server_test.rb
+++ test/unit/voicemail_server_test.rb
@@ -1,8 +1,128 @@
 require 'test_helper'
 
 class VoicemailServerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  should "be valid build" do
+    assert Factory.build( :voicemail_server ).valid?
   end
+  
+  should "not be valid with .new" do
+    assert ! VoicemailServer.new.valid?
+  end
+  
+  
+  # valid host
+  [
+    '10.0.0.0',
+    'www.amooma.de',
+    '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+    '2001:0db8:0000:08d3:0000:8a2e:0070:7344',
+    '2001:db8:0:8d3:0:8a2e:70:7344',
+    '2001:db8:0:0:0:0:1428:57ab',
+    '2001:db8::1428:57ab',
+    '2001:db8:0:0:8d3:0:0:0',
+    '2001:db8:0:0:8d3::',
+    '2001:db8::8d3:0:0:0',
+    '2001:0db8:1234::',
+    '::',  # should this be valid? (valid IPv6 address, but this is the "unspecified" address)
+    '::1', # loopback
+    'fe80::',  # link-local unicast
+    'fec0::',  # site-local unicast
+    'fc00::',  # unique local unicast
+    'fd9e:21a7:a92c:2323::1',  # unique local unicast
+    'ff00::',  # multicast
+    '192.0.2.128',
+    '::ffff:192.0.2.128',
+    '::ffff:c000:280',
+    'example.com123',
+    'abc',
+    'abc.',
+  ].each { |host|
+    should "be valid with host #{host.inspect}" do
+      assert Factory.build( :voicemail_server, :host => host ).valid?
+    end
+  }
+  
+  # invalid host
+  [
+    nil,
+    '',
+    ' ',
+    '10.0.x.0',
+    'www.amooma.01',
+    '2001:0xb8:85a3:0000:0000:8a2e:0370:7334',
+    '2001:db8::8d3::',
+    '2001:0000:85a3:0000:0000:8a2e:0370:73340',
+    '::ffff:192.0.2.128.123',
+    'example.123com',
+    '123',
+    '.',
+    ' abc',
+    'abc ',
+    '[',
+    'abc.[',
+    'a c',
+    'a#c',
+    '_',
+    '_c',
+    'a_c',
+  ].each { |host|
+    should "not be valid with host #{host.inspect}" do
+      assert ! Factory.build( :voicemail_server, :host => host ).valid?
+    end
+  }
+  
+  
+  # valid port
+  [
+    1,
+    65535,
+    nil,
+    '',
+  ].each { |port|
+    should "be valid with port #{port.inspect}" do
+      assert Factory.build( :voicemail_server, :port => port ).valid?
+    end
+  }
+  
+  # invalid port
+  [
+    'foo',
+    -1,
+    65536,
+  ].each { |port|
+    should "not be valid with port #{port.inspect}" do
+      assert ! Factory.build( :voicemail_server, :port => port ).valid?
+    end
+  }
+  
+  
+  # valid management_host
+  [
+    nil,
+    '',
+    '10.0.0.0',
+    'www.amooma.de',
+  ].each { |host|
+    should "be valid with management_host #{host.inspect}" do
+      assert Factory.build( :voicemail_server, :management_host => host ).valid?
+    end
+  }
+  
+  # invalid management_host
+  [
+    '123',
+  ].each { |host|
+    should "not be valid with management_host #{host.inspect}" do
+      assert ! Factory.build( :voicemail_server, :management_host => host ).valid?
+    end
+  }
+  
+  
+  
+  
+  
+  
+  
+  
 end
