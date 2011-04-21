5a8ff592fa32987148cddd3fed6af5d3b43dbdff
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-04-21 03:10:15 +0200

    whitespace

--- app/models/sip_account.rb
+++ app/models/sip_account.rb
@@ -7,21 +7,21 @@ class SipAccount < ActiveRecord::Base
   belongs_to :sip_server       , :validate => true
   belongs_to :sip_proxy        , :validate => true
   belongs_to :voicemail_server , :validate => true
-  belongs_to :phone        , :validate => true
+  belongs_to :phone            , :validate => true
   belongs_to :extension        , :validate => true
   belongs_to :user             , :validate => true
   
   acts_as_list :scope => :user
   
   validate_username         :auth_name
-  validates_uniqueness_of   :auth_name, :case_sensitive => false,  :scope => :sip_server_id
+  validates_uniqueness_of   :auth_name, :case_sensitive => false, :scope => :sip_server_id
   
   validates_presence_of     :sip_server
   validates_presence_of     :sip_proxy
-  validates_presence_of     :voicemail_server , :if => Proc.new { |sip_account| sip_account.voicemail_server_id }
-  validates_presence_of     :phone        , :if => Proc.new { |sip_account| sip_account.phone_id }
-  validates_presence_of     :extension        , :if => Proc.new { |sip_account| sip_account.extension_id }
-  validates_presence_of     :user             , :if => Proc.new { |sip_account| sip_account.user_id }
+  validates_presence_of     :voicemail_server , :if => Proc.new { |me| me.voicemail_server_id }
+  validates_presence_of     :phone            , :if => Proc.new { |me| me.phone_id }
+  validates_presence_of     :extension        , :if => Proc.new { |me| me.extension_id }
+  validates_presence_of     :user             , :if => Proc.new { |me| me.user_id }
   
   validate_password         :password
   
@@ -40,5 +40,5 @@ class SipAccount < ActiveRecord::Base
     :in => [ nil ],
     :if => Proc.new { |sip_account| sip_account.voicemail_server_id.blank? },
     :message => "must not be set if the SIP account does not have a voicemail server."
- 
+  
 end
