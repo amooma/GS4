79918c08af333d849e0f381cd3ca1210cc0343f1
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-04-29 17:45:18 +0200

    factories und fixtures angepaßt

--- test/factories/voicemail_servers.rb
+++ test/factories/voicemail_servers.rb
@@ -3,6 +3,4 @@
 Factory.define :voicemail_server do |f|
   f.sequence(:host) { |n| "vm-server-#{n}.localdomain" }
   f.port 4000
-  f.sequence(:management_host) { |n| "vm-server-#{n}-mgmt.localdomain" }
-  f.management_port 5000
 end
