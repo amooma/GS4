dac596bba4f19bf1baa9605d7c6a74b33b4b0593
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-04-30 19:11:22 +0200

    Nodes vervollständigt

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Gemeinschaft4::Application.routes.draw do
 	resources :sip_servers          , :only => [ :index, :show, :new, :create, :destroy ]
 	resources :sip_proxies          , :only => [ :index, :show, :new, :create, :destroy ]
 	resources :nodes                , :only => [ :index, :show ] # for now
-	# Remember to comment-in the tests once you enable more routes for the nodes.
+	# Remember to comment-in the tests in test/functional/nodes_controller_test.rb
+	# once you enable more routes for the nodes.
 	resources :voicemail_servers
 	resources :extensions
 	
