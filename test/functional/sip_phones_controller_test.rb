cfd34dee92e3247ad6a6121ec1f0154879507ec6
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-04-30 18:42:48 +0200

    provisioning_server -> node

--- test/functional/sip_phones_controller_test.rb
+++ test/functional/sip_phones_controller_test.rb
@@ -7,38 +7,7 @@ class SipPhonesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   
   setup do
-    ActiveResource::HttpMock.reset!
-    ActiveResource::HttpMock.respond_to { |mock|
-      a_cantina_phone = {
-        :id               => 0,
-        :mac_address      => '000000000000',
-        :phone_model_id   => 0,
-        :ip_address       => '0.0.0.1',
-        :last_ip_address  => nil,
-        :http_user        => '',
-        :http_password    => '',
-      }
-      first_factory_phone_phone_id = 4001
-      number_of_factory_phones = 8         # equals the number of tests (setup() is run once for each test)
-      for cantina_phone_id in (
-        (first_factory_phone_phone_id) ..
-        (first_factory_phone_phone_id + number_of_factory_phones - 1)
-      ) do
-        mock.get(    "/phones/#{cantina_phone_id.to_s}.xml", {},  # GET = show
-          a_cantina_phone.merge({
-            :id               => cantina_phone_id,
-            :mac_address      => cantina_phone_id.to_s(16).rjust(12,'0'),
-          }).to_xml( :root => "phone" ), 200, {}
-        )
-      end
-    }
-    number_of_mock_requests = ActiveResource::HttpMock.requests.length
-
     @sip_phone = Factory.create(:sip_phone)
-
-    assert( ActiveResource::HttpMock.requests.length > number_of_mock_requests,
-      "Factory.create(:sip_phone) is expected to make requests because of the phone_id validation" )
-    ActiveResource::HttpMock.reset!
     
     an_admin_username = 'admin1'
     @admin_user = User.where( :username => an_admin_username ).first
