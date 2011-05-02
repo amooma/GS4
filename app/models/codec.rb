6ce6e6fe08d78dd6eafe2cb71dd56cff94c46c88
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-03 01:26:04 +0200

    Reihenfolge

--- app/models/codec.rb
+++ app/models/codec.rb
@@ -1,7 +1,9 @@
 class Codec < ActiveRecord::Base
-  validates_presence_of :name
+  
+  validates_presence_of   :name
+  validates_format_of     :name, :with => /^[a-zA-Z0-9.\-_]{1,40}$/,
+    :allow_nil => false, :allow_blank => false
   validates_uniqueness_of :name
-  validates_format_of   :name, :with => /^[a-zA-Z0-9.\-_]{1,40}$/, :allow_nil => false, :allow_blank => false
   
   has_many :sip_account_codecs, :dependent => :destroy
   has_many :sip_accounts, :through => :sip_account_codecs
@@ -13,6 +15,7 @@ class Codec < ActiveRecord::Base
   
   private
   def downcase_codec_name
-    self.name = self.name.downcase if !self.name.blank?
+    self.name = self.name.downcase if ! self.name.blank?
   end
+  
 end
