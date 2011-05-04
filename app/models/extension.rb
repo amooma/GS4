2b0d3d92d5cf10be1fecbf7a403275cbcf5a306f
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-04 16:47:48 +0200

    Validierungen für Extension

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -7,8 +7,23 @@ class Extension < ActiveRecord::Base
   
   validates_inclusion_of  :active, :in => [ true, false ], :allow_nil => false
   
+  # Extension must not be nil or blank:
   validates_presence_of   :extension
+  # Extension must be a valid SIP "user" per RFC 3261:
+  validate_username       :extension
+  # Extension must have a format that we support:
+  validates_format_of     :extension, :with => /^[a-zA-Z0-9+*#][a-zA-Z0-9+*#.\-_]*$/,
+    :allow_nil => false, :allow_blank => false
+  # Active extensions must be unique:
   validates_uniqueness_of :extension, :scope => :active, :if => Proc.new{|me| me.active}
+  
+  # Destination must not be nil or blank:
+  validates_presence_of   :destination
+  # Destination must be a valid SIP "user" per RFC 3261:
+  validate_username       :destination
+  # Destination must have a destination that we support:
+  validates_format_of     :destination, :with => /^[a-zA-Z0-9+*#][a-zA-Z0-9+*#.\-_]*$/,
+    :allow_nil => false, :allow_blank => false
   
   
 end
