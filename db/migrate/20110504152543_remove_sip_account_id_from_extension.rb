d3f240a7d2bbd49c085b771591a913e4fde3076b
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-04 18:06:04 +0200

    doppelte Migration gelöscht und Merge-Fehler in db/schema.rb gefixt

--- db/migrate/20110504152543_remove_sip_account_id_from_extension.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSipAccountIdFromExtension < ActiveRecord::Migration
-  def self.up
-    remove_column :extensions, :sip_account_id
-  end
-
-  def self.down
-    add_column :extensions, :sip_account_id, :integer
-  end
-end
