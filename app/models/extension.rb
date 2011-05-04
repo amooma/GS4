41513866b61ee78216f4096c7c37669c75ce52da
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-04 15:47:41 +0200

    validates_presence_of :extension

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -3,7 +3,10 @@ class Extension < ActiveRecord::Base
   has_many :sip_account_to_extensions, :dependent => :destroy
   has_many :sip_accounts, :through => :sip_account_to_extensions
   
-  validates_inclusion_of :active, :in => [ true, false ], :allow_nil => false
+  validates_inclusion_of  :active, :in => [ true, false ], :allow_nil => false
+  
+  validates_presence_of   :extension
   validates_uniqueness_of :extension, :scope => :active, :if => Proc.new{|me| me.active}
+  
   
 end
