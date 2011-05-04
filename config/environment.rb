72368b09788221aed750ebe125c04c144bcced32
Peter Kozak <peter.kozak@amooma.de>
2011-05-04 14:00:13 +0200

    no db_text for subscribers by default

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Gemeinschaft4::Application.initialize!
 
-AUTH_DB_ENGINE = 'dbtext'
+AUTH_DB_ENGINE = 'odbc'
 
 if (Rails.env.production?)
-  DBTEXT_SUBSCRIBER_FILE = '/opt/kamailio-3.1/etc/kamailio/db_text/subscriber'
+  DBTEXT_SUBSCRIBER_FILE = '/etc/kamailio/db_text/subscriber'
 elsif (Rails.env.test?)
   DBTEXT_SUBSCRIBER_FILE = '/tmp/subscriber.test'
 else
