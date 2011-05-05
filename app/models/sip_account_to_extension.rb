c3f72b87ea97698aa7fea8f4ff8e4bdb0d88b732
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2011-05-05 17:58:52 +0200

    activated validations

--- app/models/sip_account_to_extension.rb
+++ app/models/sip_account_to_extension.rb
@@ -2,9 +2,9 @@ class SipAccountToExtension < ActiveRecord::Base
   belongs_to :sip_account
   belongs_to :extension
 
-  # validates_presence_of :sip_account
-  # validates_presence_of :extension
-  # validates_uniqueness_of :extension_id
+  validates_presence_of :sip_account
+  validates_presence_of :extension
+  validates_uniqueness_of :extension_id
 
   acts_as_list :scope => :sip_account
 end
