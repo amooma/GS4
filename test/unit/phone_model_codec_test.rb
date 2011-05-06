65bc89a0413050179b428c240607ca4a78b5d427
Peter Kozak <peter.kozak@amooma.de>
2011-05-06 10:48:03 +0200

    phone_model_codec unit test extended

--- test/unit/phone_model_codec_test.rb
+++ test/unit/phone_model_codec_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class PhoneModelCodecTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  should "be valid" do
+    assert Factory.build(:phone_model_codec).valid?
   end
+  
+  # A codec must be unique
+  #
+  should "not be valid with a not unique name" do
+    phone_model = Factory.create(:phone_model)
+    codec1 = Factory.create(:codec)
+    codec2 = Factory.create(:codec)
+    phone_model.codecs << codec1
+    phone_model.codecs << codec2
+    assert !phone_model.codecs.build(Factory.attributes_for(:codec, :name => codec1.name)).valid?
+  end
+  
 end
