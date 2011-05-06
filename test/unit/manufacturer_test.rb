c83b3834a7fe260b329b0834d8809a1c22355027
Peter Kozak <peter.kozak@amooma.de>
2011-05-06 09:55:41 +0200

    manufacturer unit test added

--- test/unit/manufacturer_test.rb
+++ test/unit/manufacturer_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class ManufacturerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  should "be valid" do
+    assert Factory.build(:manufacturer).valid?
+  end
+  
+  # A manufacturer name must be unique
+  #
+  should "not be valid with a not unique name" do
+    manufacturer = Factory.create(:manufacturer)
+    assert !Factory.build(:manufacturer, :name => manufacturer.name).valid?
+  end
+  
+  # A manufacturer has to have a name
+  #
+  should "not be valid with nil name" do
+    manufacturer = Factory.create(:manufacturer)
+    assert !Factory.build(:manufacturer, :name => nil).valid?
+  end
+  
+  should "not be valid with an empty name" do
+    manufacturer = Factory.create(:manufacturer)
+    assert !Factory.build(:manufacturer, :name => '').valid?
   end
 end
