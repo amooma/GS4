1cdc133120da14ade72037523a60b5f16ced7621
Sascha Daniels <sd@alternative-solution.de>
2011-05-10 15:58:26 +0200

    Validations

--- app/models/call_forward.rb
+++ app/models/call_forward.rb
@@ -1,4 +1,9 @@
 class CallForward < ActiveRecord::Base
   belongs_to :call_forward_reason
   belongs_to :sip_account
+  
+  validates_presence_of :sip_account
+  validates_presence_of :call_forward_reason
+  validates_presence_of :destination
+  validates_uniqueness_of :call_forward_reason_id, :scope => :sip_account_id
 end
