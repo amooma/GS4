c7766dc1a26079fbea7ccc5a8707586c8c146864
root <root@c3po.(none)>
2011-05-16 16:42:48 +0200

    Seeds for call parking

--- db/seeds.rb
+++ db/seeds.rb
@@ -345,6 +345,8 @@ Version.create(:table_name => "purplemap", :table_version => "1")
 Version.create(:table_name => "uacreg", :table_version => "1")
 
 Extension.create(:extension => "80", :destination => "-vmmenue-", :active => true)
+Extension.create(:extension => "90", :destination => "-park-in-", :active => true)
+Extension.create(:extension => "99", :destination => "-park-out-", :active => true)
 
 call_forward_reasons = ['busy', 'noanswer', 'offline', 'always']
 
