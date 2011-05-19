6c4f69e8d37d55086d835c4a5b4fc85f36076cdf
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-19 12:10:26 +0200

    #OPTIMIZE Is this controller actually used for anything?

--- app/controllers/phone_book_internal_user_controllers.rb
+++ app/controllers/phone_book_internal_user_controllers.rb
@@ -1,5 +1,6 @@
 class PhoneBookInternalUsersController < ApplicationController
   
+  #OPTIMIZE Is this controller actually used for anything?
   #TODO Authentication and authorization.
   
   def index
