2662ab16bbcc36a88bcdd3d01fc439ab61c879b7
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-19 12:57:49 +0200

    whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery
   
   before_filter { |controller|
     @app_number_of_users = User.count
   }
+  
   def local_ip
-    UDPSocket.open {|s| s.connect("255.255.255.254", 1); s.addr.last}
+    UDPSocket.open {|s| s.connect("255.255.255.254", 1); s.addr.last }
   end
+  
 end
