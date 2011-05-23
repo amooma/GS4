755fffabc7d6a635dd449b5c13602560512cf005
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-05-23 20:18:01 +0200

    empty?() -> blank?()

--- app/models/call_forward.rb
+++ app/models/call_forward.rb
@@ -5,7 +5,7 @@ class CallForward < ActiveRecord::Base
   
   validates_presence_of :sip_account
   validates_presence_of :call_forward_reason
-  validates_presence_of :destination , :if  => Proc.new { |cf| cf.source.empty? }
+  validates_presence_of :destination , :if  => Proc.new { |cf| cf.source.blank? }
   validates_uniqueness_of :call_forward_reason_id, :scope => [ :sip_account_id, :source ]
   
 end
