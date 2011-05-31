60ab5cb2f111cd647ae641c4dba48ce7aa03fdf5
Peter Kozak <peter.kozak@amooma.de>
2011-05-31 12:57:26 +0200

    fetching server based FreeSWITCH configuration improved

---
+++ misc/freeswitch/fs-scripts/freeswitch-gemeinschaft4.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -e
+cd /opt/freeswitch/conf/
+wget \
+        -O freeswitch-gemeinschaft4.xml \
+        --dns-timeout=10 \
+        --connect-timeout=10 \
+        --read-timeout=60 \
+        --tries=30 \
+        --retry-connrefused \
+        --waitretry=10 \
+        http://127.0.0.1/freeswitch-configuration/freeswitch.xml \
+        1>>/dev/null 2>>/dev/null || true
+cat freeswitch-gemeinschaft4.xml
