cf6f4de892963ba0e0a5322d8ea3bf0e7a44cb47
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-07 20:27:01 +0200

    gefixt: #OPTIMIZE Do not allow "anonymous" as an extension. It is reserved. (RFC 2543, RFC 3325).

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -5,27 +5,41 @@ class Extension < ActiveRecord::Base
   has_many :conference_to_extensions, :dependent => :destroy
   has_many :conferences, :through => :conference_to_extensions
   
+  
   validates_inclusion_of  :active, :in => [ true, false ], :allow_nil => false
+  
   
   # Extension must not be nil or blank:
   validates_presence_of   :extension
+  
   # Extension must be a valid SIP "user" per RFC 3261:
   validate_username       :extension
+  
   # Extension must have a format that we support:
-  validates_format_of     :extension, :with => /^[a-zA-Z0-9+*#][a-zA-Z0-9+*#.\-_]*$/,
-    :allow_nil => false, :allow_blank => false
+  validates_format_of     :extension, :with => /^[a-zA-Z0-9+*][a-zA-Z0-9+*.\-_]*$/,
+    :allow_nil   => false,
+    :allow_blank => false
+  
+  # Do not allow "anonymous" as an extension. It is reserved. (RFC 2543, RFC 3325)  
+  validates_exclusion_of  :extension, :in => [ 'anonymous' ]
+  
   # Active extensions must be unique:
   validates_uniqueness_of :extension, :scope => :active, :if => Proc.new{|me| me.active}
   
-  #OPTIMIZE Do not allow "anonymous" as an extension. It is reserved. (RFC 2543, RFC 3325)
-  # And add a test that makes sure "anonymous" is invalid.
   
   # Destination must not be nil or blank:
   validates_presence_of   :destination
+  
   # Destination must be a valid SIP "user" per RFC 3261:
   validate_username       :destination
+  
   # Destination must have a destination that we support:
-  validates_format_of     :destination, :with => /^[a-zA-Z0-9+*#\-][a-zA-Z0-9+*#.\-_]*$/,
-    :allow_nil => false, :allow_blank => false
+  validates_format_of     :destination, :with => /^[a-zA-Z0-9+*\-][a-zA-Z0-9+*.\-_]*$/,
+    :allow_nil   => false,
+    :allow_blank => false
+  
+  # Do not allow "anonymous" as a destination. It is reserved. (RFC 2543, RFC 3325)  
+  validates_exclusion_of  :destination, :in => [ 'anonymous' ]
+  
 end
 
