fed48d4d933fd9156ca387e7d9dea39591eddfad
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-13 04:03:49 +0200

    diverse Tests gefixt

--- test/functional/subscribers_controller_test.rb
+++ test/functional/subscribers_controller_test.rb
@@ -1,18 +1,44 @@
 require 'test_helper'
 
 class SubscribersControllerTest < ActionController::TestCase
+  
   setup do
-    @subscriber = Factory.create(:subscriber) 
+    @subscriber = Factory.create(:subscriber)
+    
+    an_admin_username = 'admin1'
+    @admin_user = User.where( :username => an_admin_username ).first
+    assert_not_nil( @admin_user, "This tests needs user #{an_admin_username.inspect}" )
+    
+    #@expected_http_status_if_not_allowed = 403
+    @expected_http_status_if_not_allowed = 302
   end
-
+  
+  
   test "should get index" do
+    sign_in :user, @admin_user
     get :index
     assert_response :success
     assert_not_nil assigns(:subscribers)
+    sign_out @admin_user
   end
-
+  
+  test "should not get index (not an admin)" do
+    get :index
+    assert_response( @expected_http_status_if_not_allowed )
+  end
+  
+  
   test "should show subscriber" do
+    sign_in :user, @admin_user
     get :show, :id => @subscriber.to_param
     assert_response :success
+    sign_out @admin_user
   end
+  
+  test "should not show subscriber (not an admin)" do
+    get :show, :id => @subscriber.to_param
+    assert_response( @expected_http_status_if_not_allowed )
+  end
+  
+  
 end
