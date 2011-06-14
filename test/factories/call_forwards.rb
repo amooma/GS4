1b416b2627de782106c739e7c62c72291c72f4b2
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-14 19:37:34 +0200

    Fixtures

--- test/factories/call_forwards.rb
+++ test/factories/call_forwards.rb
@@ -1,5 +1,6 @@
 Factory.define :call_forward do |f|
   f.association :sip_account
+  #f.sequence( :call_forward_reason_id ) { |n| CallForwardReason.where(:value => "always").first.id }
   f.association :call_forward_reason
   f.sequence( :destination  ) { |n| "#{n}" }
   f.sequence( :call_timeout ) { |n| nil }
