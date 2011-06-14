c96d481ea839fa8bf2e9486ece474365ce60b048
Peter Kozak <peter.kozak@amooma.de>
2011-06-14 15:00:49 +0200

    call forward factory

--- test/unit/call_forward_test.rb
+++ test/unit/call_forward_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class CallForwardTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+	should "have a valid factory" do
+		assert Factory.build(:call_forward).valid?
+	end
+	
+	# destinations
+	[
+		'12345',
+		'abcde',
+		'ABCDE',
+	].each { |destination|
+		should "be possible to set destination to #{destination.inspect}" do
+			assert Factory.build( :call_forward, :destination => destination ).valid?
+		end
+	}
+	
+	call_forward_reason = Factory.build( :call_forward_reason, :value => 'noanswer' )
+	# reason "noanswer" and timeout
+	[
+		1,
+		60,
+		120,
+		121,
+	].each { |timeout|
+		should "be possible to set timeout to #{timeout.inspect}" do
+			assert Factory.build( :call_forward, :call_timeout => timeout, :call_forward_reason =>  call_forward_reason).valid?
+		end
+	}
+
 end
