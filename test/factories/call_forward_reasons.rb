296589726a2a9b8c91e4f3b511be511f24848f69
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-14 19:28:41 +0200

    Factory.define :call_forward_reason

--- test/factories/call_forward_reasons.rb
+++ test/factories/call_forward_reasons.rb
@@ -1,5 +1,5 @@
 Factory.define :call_forward_reason do |f|
   #reasons = [ 'busy', 'noanswer', 'offline', 'always', 'assistant' ]
   reasons = [ 'busy', 'offline', 'always' ]
-  f.sequence( :value ) { |n| "#{reasons[n % 5]}" }
+  f.sequence( :value ) { |n| "#{reasons[ n % reasons.count ]}" }
 end
