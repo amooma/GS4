287815612d529de595c44422ea1bba2c8cc672bd
Sascha Daniels <sd@alternative-solution.de>
2011-06-14 12:35:52 +0200

    Rename type to call_type in call_logs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110614091546) do
+ActiveRecord::Schema.define(:version => 20110614101730) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -41,11 +41,11 @@ ActiveRecord::Schema.define(:version => 20110614091546) do
     t.integer  "sip_account_id"
     t.string   "source"
     t.string   "destination"
-    t.string   "type"
     t.string   "disposition"
     t.string   "forwarded_to"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "call_type"
   end
 
   create_table "call_queue_to_extensions", :force => true do |t|
