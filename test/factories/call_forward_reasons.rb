ce4cd18fd8484b05e9f3040a741d4d610e8d2cc5
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-14 17:52:53 +0200

    CallForwardsControllerTest

--- test/factories/call_forward_reasons.rb
+++ test/factories/call_forward_reasons.rb
@@ -1,4 +1,5 @@
 Factory.define :call_forward_reason do |f|
-  reasons = ['busy', 'noanswer', 'offline', 'always', 'assistant']
+  #reasons = [ 'busy', 'noanswer', 'offline', 'always', 'assistant' ]
+  reasons = [ 'busy', 'offline', 'always' ]
   f.sequence( :value ) { |n| "#{reasons[n % 5]}" }
 end
--- test/factories/call_forwards.rb
+++ test/factories/call_forwards.rb
@@ -1,8 +1,8 @@
 Factory.define :call_forward do |f|
-  f.sequence( :destination ) { |n| "#{n}" }
-  f.sequence( :call_timeout ) { |n| n }
-  f.sequence( :source ) { |n| nil }
-  
   f.association :sip_account
   f.association :call_forward_reason
+  f.sequence( :destination  ) { |n| "#{n}" }
+  f.sequence( :call_timeout ) { |n| nil }
+  f.sequence( :source       ) { |n| "#{n}" }
+  f.sequence( :active       ) { |n| false }
 end
