3187c64f8274b85a740c29ebda3b4d6b3f86db8f
Sascha Daniels <sd@alternative-solution.de>
2011-06-15 23:27:30 +0200

    FS needs to write logs from localhost

--- test/functional/call_logs_controller_test.rb
+++ test/functional/call_logs_controller_test.rb
@@ -26,12 +26,7 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not get index (not an admin)" do
-    get :index
-    assert_response( @expected_http_status_if_not_allowed )
-  end
-  
-  
+    
   test "should get new" do
     sign_in :user, @admin_user
     get :new
@@ -39,10 +34,6 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not get new (not an admin)" do
-    get :new
-    assert_response( @expected_http_status_if_not_allowed )
-  end
   
   
   test "should create call_log" do
@@ -55,12 +46,7 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not create call_log (not an admin)" do
-    assert_no_difference('CallLog.count') {
-      post :create, :call_log => Factory.attributes_for(:call_log)
-    }
-    assert_response( @expected_http_status_if_not_allowed )
-  end
+  
   
   
   test "should show call_log" do
@@ -70,10 +56,7 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not show call_log (not an admin)" do
-    get :show, :id => @call_log.to_param
-    assert_response( @expected_http_status_if_not_allowed )
-  end
+  
   
   
   test "should get edit" do
@@ -83,10 +66,7 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not get edit (not an admin)" do
-    get :edit, :id => @call_log.to_param
-    assert_response( @expected_http_status_if_not_allowed )
-  end
+  
   
   
   test "should update call_log" do
@@ -96,10 +76,7 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not update call_log (not an admin)" do
-    put :update, :id => @call_log.to_param, :call_log => @call_log.attributes
-    assert_response( @expected_http_status_if_not_allowed )
-  end
+  
   
   
   test "should destroy call_log" do
@@ -111,11 +88,6 @@ class CallLogsControllerTest < ActionController::TestCase
     sign_out @admin_user
   end
   
-  test "should not destroy call_log (not an admin)" do
-    assert_no_difference('CallLog.count') {
-      delete :destroy, :id => @call_log.to_param
-    }
-    assert_response( @expected_http_status_if_not_allowed )
-  end
+ 
   
 end
