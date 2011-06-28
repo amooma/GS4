4fa19a7eacca27927e338b3716fe4df7bb93b374
Peter Kozak <peter.kozak@amooma.de>
2011-06-28 11:48:12 +0200

    nodes controller localization

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -47,7 +47,7 @@ class NodesController < ApplicationController
     
     respond_to do |format|
       if @node.save
-        format.html { redirect_to(@node, :notice => 'Node was successfully created.') }
+        format.html { redirect_to(@node, :notice => t(:node_created)) }
         format.xml  { render :xml => @node, :status => :created, :location => @node }
       else
         format.html { render :action => "new" }
@@ -63,7 +63,7 @@ class NodesController < ApplicationController
     
     respond_to do |format|
       if @node.update_attributes(params[:node])
-        format.html { redirect_to(@node, :notice => 'Node was successfully updated.') }
+        format.html { redirect_to(@node, :notice => t(:node_updated)) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
