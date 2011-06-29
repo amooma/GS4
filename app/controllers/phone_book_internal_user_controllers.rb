c8066f9a476c06ad27169741f3eb6cadc03762df
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-29 14:01:27 +0200

    typo and whitespace

--- app/controllers/phone_book_internal_user_controllers.rb
+++ app/controllers/phone_book_internal_user_controllers.rb
@@ -6,7 +6,7 @@ class PhoneBookInternalUsersController < ApplicationController
   def index
     @sip_accounts = SipAccount.all
     respond_to { |format|
-     ormat.xml  { render :xml => @sip_accounts }
+      format.xml  { render :xml => @sip_accounts }
 	} 
   end
   
