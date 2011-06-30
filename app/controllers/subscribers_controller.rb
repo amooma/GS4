8ffaa8ec1edd97faddee8de2ee502fefbad8efe3
Peter Kozak <peter.kozak@amooma.de>
2011-06-30 10:38:40 +0200

    subscriber controller - deleted unused handlers

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -39,54 +39,5 @@ class SubscribersController < ApplicationController
       format.xml  { render :xml => @subscriber }
     end
   end
-
-  # GET /subscribers/1/edit
-  def edit
-    @subscriber = Subscriber.find(params[:id])
-  end
-
-  # POST /subscribers
-  # POST /subscribers.xml
-  def create
-    @subscriber = Subscriber.new(params[:subscriber])
-
-    respond_to do |format|
-      if @subscriber.save
-        format.html { redirect_to(@subscriber, :notice => 'Subscriber was successfully created.') }
-        format.xml  { render :xml => @subscriber, :status => :created, :location => @subscriber }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @subscriber.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /subscribers/1
-  # PUT /subscribers/1.xml
-  def update
-    @subscriber = Subscriber.find(params[:id])
-
-    respond_to do |format|
-      if @subscriber.update_attributes(params[:subscriber])
-        format.html { redirect_to(@subscriber, :notice => 'Subscriber was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @subscriber.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /subscribers/1
-  # DELETE /subscribers/1.xml
-  def destroy
-    @subscriber = Subscriber.find(params[:id])
-    @subscriber.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(subscribers_url) }
-      format.xml  { head :ok }
-    end
-  end
   
 end
