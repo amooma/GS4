469e2e0ae4f624adc78c050c35f59861b7274554
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-06-30 16:22:33 +0200

    whitespace

--- app/controllers/fax_documents_controller.rb
+++ app/controllers/fax_documents_controller.rb
@@ -1,12 +1,12 @@
 class FaxDocumentsController < ApplicationController
   
   before_filter :authenticate_user!, :unless => Proc.new { |r| r.request.remote_ip   == '127.0.0.1' }
-
+  
   # GET /fax_documents
   # GET /fax_documents.xml
   def index
     @fax_documents = FaxDocument.all
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @fax_documents }
@@ -17,17 +17,17 @@ class FaxDocumentsController < ApplicationController
   # GET /fax_documents/1.xml
   def show
     @fax_document = FaxDocument.find(params[:id])
-	
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @fax_document }
-	  format.tif { 
+      format.tif { 
         send_file "#{FAX_FILES_DIRECTORY}/#{@fax_document.raw_file}.tif", :type => "image/tiff", 
-		  :filename => File.basename(@fax_document.file, File.extname(@fax_document.file)) + '.tif'
-	  }
-	  format.png {
+          :filename => File.basename(@fax_document.file, File.extname(@fax_document.file)) + '.tif'
+      }
+      format.png {
         send_file "#{FAX_FILES_DIRECTORY}/#{@fax_document.raw_file}.png", :type => "image/png", :disposition => 'inline', 
-		  :filename => File.basename(@fax_document.file, File.extname(@fax_document.file)) + '.png'
+          :filename => File.basename(@fax_document.file, File.extname(@fax_document.file)) + '.png'
       }
     end
   end
@@ -36,7 +36,7 @@ class FaxDocumentsController < ApplicationController
   # GET /fax_documents/new.xml
   def new
     @fax_document = FaxDocument.new
-
+    
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @fax_document }
@@ -52,8 +52,8 @@ class FaxDocumentsController < ApplicationController
   # POST /fax_documents.xml
   def create
     @fax_document = FaxDocument.new(params[:fax_document])
-	#@fax_document.save_file(params[:fax_document])
-	
+    #@fax_document.save_file(params[:fax_document])
+    
     respond_to do |format|
       if  @fax_document.save
         format.html { redirect_to(@fax_document, :notice => 'Fax document was successfully created.') }
@@ -69,7 +69,7 @@ class FaxDocumentsController < ApplicationController
   # PUT /fax_documents/1.xml
   def update
     @fax_document = FaxDocument.find(params[:id])
-
+    
     respond_to do |format|
       if @fax_document.update_attributes(params[:fax_document])
         format.html { redirect_to(@fax_document, :notice => 'Fax document was successfully updated.') }
@@ -86,10 +86,11 @@ class FaxDocumentsController < ApplicationController
   def destroy
     @fax_document = FaxDocument.find(params[:id])
     @fax_document.destroy
-
+    
     respond_to do |format|
       format.html { redirect_to(fax_documents_url) }
       format.xml  { head :ok }
     end
   end
+  
 end
