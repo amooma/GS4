a07f0aecee3a12624f4165cafec47bcdc7e0549d
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-03 19:17:45 +0200

    #OPTIMIZE Looks like this should rather be called PersonalPhonebookEntries.

--- app/models/personal_phonebook.rb
+++ app/models/personal_phonebook.rb
@@ -1,4 +1,5 @@
 class PersonalPhonebook < ActiveRecord::Base
+  #OPTIMIZE Looks like this should rather be called PersonalPhonebookEntries.
   belongs_to :user, :validate => true
   validates_presence_of :user
 end
