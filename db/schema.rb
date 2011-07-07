ff7edcd67e44c1ccb45c29a7f8ff879bf0f5213b
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-07 18:32:33 +0200

    bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -345,19 +345,6 @@ ActiveRecord::Schema.define(:version => 20110705115200) do
     t.datetime "updated_at"
   end
 
-  create_table "user_group_members", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "user_group_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "user_groups", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
