fa03588d36c03e1f8d7be6d832d0400487145c2f
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-07 18:39:55 +0200

    whitespace

--- app/models/sip_account.rb
+++ app/models/sip_account.rb
@@ -46,15 +46,14 @@ class SipAccount < ActiveRecord::Base
   before_validation {
     if self.phone_id
       if self.user_id != nil
-        if  ! SipAccount.find(:all, :conditions => ['user_id != ? AND phone_id = ? ', "#{self.user_id}", "#{self.phone_id}"]).empty? ||
-          ! SipAccount.where(:user_id => nil, :phone_id => self.phone_id).empty?
-            errors.add( :base, I18n.t(:phone_belongs_to_user_allready) )
+        if ! SipAccount.find(:all, :conditions => ['user_id != ? AND phone_id = ? ', "#{self.user_id}", "#{self.phone_id}"]).empty? \
+        || ! SipAccount.where(:user_id => nil, :phone_id => self.phone_id).empty?
+          errors.add( :base, I18n.t(:phone_belongs_to_user_allready) )
         end
       else
-        if  ! SipAccount.find(:all, :conditions => ['user_id NOT ? AND phone_id = ?', nil, "#{self.phone_id}" ]).empty?
-        errors.add( :base, I18n.t(:phone_belongs_to_user_allready) )
+        if ! SipAccount.find(:all, :conditions => ['user_id NOT ? AND phone_id = ?', nil, "#{self.phone_id}" ]).empty?
+          errors.add( :base, I18n.t(:phone_belongs_to_user_allready) )
         end
-        
       end
     end
   }
