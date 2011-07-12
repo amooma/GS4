bcfee66d84b9b95246f122a251f372a1178c6f03
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-12 11:37:42 +0200

    Alte Migration db/migrate/20110630104635_create_personal_phoneboook.rb wiederhergestellt da sonst ein Fehler beim Löschen der Tabelle auftritt (SQLite3::SQLException: no such table: personal_phonebooks: DROP TABLE "personal_phonebooks")

---
+++ db/migrate/20110630104635_create_personal_phoneboook.rb
@@ -0,0 +1,17 @@
+class CreatePersonalPhoneboook < ActiveRecord::Migration
+	
+	def self.up
+		create_table :personal_phonebooks do |t|
+			t.integer :user_id
+			t.string :first_name
+			t.string :last_name
+			t.string :phone_number
+			
+			t.timestamps
+		end
+	end
+	
+	def self.down
+		drop_table :personal_phonebooks
+	end
+end
