439ed597713329a416af854eb9f545e845ff8253
Peter Kozak <peter.kozak@amooma.de>
2011-07-21 10:21:01 +0200

    configuration - pass default value to getter method

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -2,11 +2,11 @@ class Configuration < ActiveRecord::Base
 	validates_presence_of   :name
 	validates_uniqueness_of :name
 	
-	def self.get( name )
+	def self.get( name, default_value = nil)
 		begin
 			return self.where(:name => name).first.value
 		rescue
-			return nil
+			return default_value
 		end
 	end
 end
