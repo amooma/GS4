6c83c8188785e2ca447e2a892167da483230cfee
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-21 18:35:10 +0200

    ws

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,8 +1,9 @@
 class Configuration < ActiveRecord::Base
+	
 	validates_presence_of   :name
 	validates_uniqueness_of :name
 	
-	def self.get( name, default_value = nil)
+	def self.get( name, default_value = nil )
 		begin
 			return self.where(:name => name).first.value
 		rescue
