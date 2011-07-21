91f6d399a88016cd616b33f44da921dbd8d23dab
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-21 19:19:56 +0200

    #OPTIMIZE Es muss noch im model abgefangen werden, was nicht gelöscht werden darf, siehe db/seeds.rb.

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -3,6 +3,8 @@ class Configuration < ActiveRecord::Base
 	validates_presence_of   :name
 	validates_uniqueness_of :name
 	
+	#OPTIMIZE Es muss noch im model abgefangen werden, was nicht gelöscht werden darf, siehe db/seeds.rb.
+	
 	def self.get( name, default_value = nil )
 		begin
 			return self.where(:name => name).first.value
