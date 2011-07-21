fa755b59446d1f9b73c3a2a727555d1dd2b4b861
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-21 18:45:32 +0200

    ws

--- test/functional/configurations_controller_test.rb
+++ test/functional/configurations_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ConfigurationsControllerTest < ActionController::TestCase
+  
   setup do
     @configuration = Factory.create(:configuration)
 
@@ -11,61 +12,60 @@ class ConfigurationsControllerTest < ActionController::TestCase
     #@expected_http_status_if_not_allowed = 403
     @expected_http_status_if_not_allowed = 302
   end
- 
+  
   test "should get index" do
     sign_in :user, @admin_user
     get :index
     assert_response :success
     assert_not_nil assigns(:configurations)
     sign_out @admin_user
- end
-
+  end
+  
   test "should get new" do
     sign_in :user, @admin_user
     get :new
     assert_response :success
     sign_out @admin_user
   end
-
+  
   test "should create configuration" do
     sign_in :user, @admin_user
     assert_difference('Configuration.count') do
       configuration = Factory.build(:configuration)
       post :create, :configuration => configuration.attributes
     end
-
     assert_redirected_to configuration_path(assigns(:configuration))
     sign_out @admin_user
   end
-
+  
   test "should show configuration" do
     sign_in :user, @admin_user
     get :show, :id => @configuration.to_param
     assert_response :success
     sign_out @admin_user
   end
-
+  
   test "should get edit" do
     sign_in :user, @admin_user
     get :edit, :id => @configuration.to_param
     assert_response :success
     sign_out @admin_user
   end
-
+  
   test "should update configuration" do
     sign_in :user, @admin_user
     put :update, :id => @configuration.to_param, :configuration => @configuration.attributes
     assert_redirected_to configuration_path(assigns(:configuration))
     sign_out @admin_user
   end
-
+  
   test "should destroy configuration" do
     sign_in :user, @admin_user
     assert_difference('Configuration.count', -1) do
       delete :destroy, :id => @configuration.to_param
     end
-
     assert_redirected_to configurations_path
     sign_out @admin_user
   end
+  
 end
