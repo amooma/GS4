661b5b015ad944a8e74483721b4b15de0613211b
Peter Kozak <peter.kozak@amooma.de>
2011-07-22 22:36:02 +0200

    configuration caching

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -3,18 +3,30 @@ class Configuration < ActiveRecord::Base
 	validates_presence_of   :name
 	validates_uniqueness_of :name
 	
-	#OPTIMIZE Es muss noch im model abgefangen werden, was nicht gelöscht werden darf, siehe db/seeds.rb. - Vielleicht benötgen wir noch Flags im Modell, welcher Eintrag gelöscht und welcher nur verändet aber nicht ganz gelöscht werden darf. - Ja, das wäre imho sauber.
+	after_save {
+		@@conf.clear
+	}
+
+	@@conf = Hash.new()
 	
-	def self.get( name, default_value = nil )
-		begin
-			return self.where(:name => name).first.value
-		rescue
-			return default_value ? default_value.to_s : nil
+	#OPTIMIZE add do_not_delete flag to model
+	
+	def self.get( name, default_value = nil)
+		if @@conf.key?( name )
+			return @@conf[name]
 		end
 		
-		#OPTIMIZE Don't use exceptions for normal control flow.
-		#config = self.where(:name => name).first
-		#return config.value if config
-		#return default_value ? default_value.to_s : nil
+		config_entry = self.where(:name => name).first
+		if config_entry
+			@@conf[name] = config_entry.value
+			return config_entry.value
+		else
+			@@conf[name] = default_value.to_s
+			return @@conf[name]
+		end
+	end
+
+	def self.cache_clear()
+		@@conf.clear
 	end
 end
