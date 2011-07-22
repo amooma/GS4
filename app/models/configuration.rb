b78d17d73b71bcb0403004bacb51c1a581baaf76
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-07-22 23:10:21 +0200

    ws, kürzer und korrektes Verhalten bei keinem Default-Wert (=> nil) wiederhergestellt

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -6,12 +6,12 @@ class Configuration < ActiveRecord::Base
 	after_save {
 		@@conf.clear
 	}
-
+	
 	@@conf = Hash.new()
 	
-	#OPTIMIZE add do_not_delete flag to model
+	#OPTIMIZE Add do_not_delete flag to model, see db/seeds.rb
 	
-	def self.get( name, default_value = nil)
+	def self.get( name, default_value = nil )
 		if @@conf.key?( name )
 			return @@conf[name]
 		end
@@ -19,14 +19,14 @@ class Configuration < ActiveRecord::Base
 		config_entry = self.where(:name => name).first
 		if config_entry
 			@@conf[name] = config_entry.value
-			return config_entry.value
 		else
-			@@conf[name] = default_value.to_s
-			return @@conf[name]
+			@@conf[name] = default_value ? default_value.to_s : nil
 		end
+		return @@conf[name]
 	end
 
 	def self.cache_clear()
 		@@conf.clear
 	end
+	
 end
