5370ccb35c73da7612547f1e641481316d3eab69
Peter Kozak <peter.kozak@amooma.de>
2011-07-27 09:36:25 +0200

    optimizations

--- app/models/fax_document.rb
+++ app/models/fax_document.rb
@@ -12,15 +12,16 @@ class FaxDocument < ActiveRecord::Base
 			self.create_raw_file_from_document()
 		end
 		if (self.raw_file)
-			raw_file = "#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.tif"
+			raw_file =  File.expand_path("#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.tif")
+	
 			if (! self.raw_file || ! File.exists?(raw_file))
 				errors.add( :base, I18n.t(:fax_document_not_found))
 			else
-				thumbnail = "#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.png"
-				if (! File.exists?(thumbnail))
-					thumbnail_size = Configuration.get(:fax_thumbnail_size, '210x310')
-					system "convert -resize #{thumbnail_size}\! #{Configuration.get(:fax_files_directory)}/#{raw_file}.tif #{Configuration.get(:fax_files_directory)}/#{raw_file}.png"
-					#OPTIMIZE Protect against bad strings from Configuration.get() by shell-escaping the arguments.
+				thumbnail_file = File.expand_path("#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.png")
+	
+				if (! File.exists?(thumbnail_file))
+					thumbnail_size = "#{Configuration.get(:fax_thumbnail_width, 210, Integer)}x#{Configuration.get(:fax_thumbnail_height, 310, Integer)}"
+					system "convert -resize #{thumbnail_size}\! \"#{raw_file}\" \"#{thumbnail_file}\""
 				end
 				if (! self.destination.blank?)
 					originate_call(self.destination, raw_file)
@@ -33,7 +34,7 @@ class FaxDocument < ActiveRecord::Base
 	
 	after_validation( :on => :update ) {
 		if outgoing
-			raw_file = "#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.tif"
+			raw_file =  File.expand_path("#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.tif")
 			if (! self.raw_file || ! File.exists?(raw_file))
 				errors.add( :base, I18n.t(:fax_document_not_found))
 			elsif (! self.destination.blank?)
@@ -43,13 +44,14 @@ class FaxDocument < ActiveRecord::Base
 	}
 	
 	before_destroy {
-		raw_file = "#{Configuration.get(:fax_files_directory)}/#{self.raw_file}"
+		raw_file =  File.expand_path("#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.tif")
 		begin
-			File.unlink( raw_file + '.tif' )
+			File.unlink( raw_file )
 		rescue
 		end
+		thumbnail_file = File.expand_path("#{Configuration.get(:fax_files_directory)}/#{self.raw_file}.png")
 		begin
-			File.unlink( raw_file + '.png' )
+			File.unlink( thumbnail_file )
 		rescue
 		end
 	}
@@ -69,44 +71,43 @@ class FaxDocument < ActiveRecord::Base
 			errors.add( :base, I18n.t(:fax_document_not_found))
 			return false
 		end
-		resolution =  Configuration.get(:fax_default_resolution, '204x98')
-		page_size = Configuration.get(:fax_default_page_size, '1728x1078')
+		resolution =  "#{Configuration.get(:fax_default_horizontal_resolution, 204, Integer)}x#{Configuration.get(:fax_default_vertical_resolution, 98, Integer)}"
+		page_size = "#{Configuration.get(:fax_default_page_width, 1728, Integer)}x#{Configuration.get(:fax_default_page_height, 1078, Integer)}"
 		
-		output_file = "FAX-OUT-#{SecureRandom.hex(10)}"
-		output_path = "#{Configuration.get(:fax_files_directory)}/#{output_file}"
-		if (File.exist?(output_path+'.tif'))
+		file_base_name = "#{File.basename(Configuration.get(:fax_outgoing_file_prefix, 'fax_out_'))}#{SecureRandom.hex(10)}"
+		raw_file =  File.expand_path("#{Configuration.get(:fax_files_directory)}/#{file_base_name}.tif")
+		thumbnail_file =  File.expand_path("#{Configuration.get(:fax_files_directory)}/#{file_base_name}.png")
+		
+		if (File.exist?(raw_file))
 			errors.add( :base, I18n.t(:file_exists))
 			return false
 		end
 		
 		#create fax g3 file
-		system "gs -q -r#{resolution} -g#{page_size} -dNOPAUSE -dBATCH -dSAFER -sDEVICE=tiffg3 -sOutputFile=#{output_path}.tif -- #{input_file}"
-		#OPTIMIZE Protect against bad strings from Configuration.get() by shell-escaping the arguments.
-		if (! File.exist?(output_path+'.tif'))
-			system "convert -density #{resolution} -resize #{page_size}\! -monochrome -compress Fax #{input_file} #{output_path}.tif"
-			#OPTIMIZE Protect against bad strings from Configuration.get() by shell-escaping the arguments.
+		system "gs -q -r#{resolution} -g#{page_size} -dNOPAUSE -dBATCH -dSAFER -sDEVICE=tiffg3 -sOutputFile=\"#{raw_file}\" -- \"#{input_file}\""
+		if (! File.exist?(raw_file))
+			system "convert -density #{resolution} -resize #{page_size}\! -monochrome -compress Fax \"#{input_file}\" \"#{raw_file}\""
 		end
 		
-		thumbnail_resolution = Configuration.get(:fax_thumbnail_resolution, '21x31')
-		thumbnail_size = Configuration.get(:fax_thumbnail_size, '210x310')
+		thumbnail_resolution = "#{Configuration.get(:fax_thumbnail_horizontal_resolution, 21, Integer)}x#{Configuration.get(:fax_thumbnail_vertical_resolution, 31, Integer)}"
+		thumbnail_size = "#{Configuration.get(:fax_thumbnail_width, 210, Integer)}x#{Configuration.get(:fax_thumbnail_height, 310, Integer)}"
 		
 		#create fax png thumbnail
-		system "gs -q -r#{thumbnail_resolution} -g#{thumbnail_size} -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pngmono -sOutputFile=#{output_path}.png -- #{input_file}"
-		#OPTIMIZE Protect against bad strings from Configuration.get() by shell-escaping the arguments.
-		if (File.exist?(output_path+'.tif') && ! File.exist?(output_path+'.png'))
-			system "convert -resize #{thumbnail_size}\! #{output_path}.tif #{output_path}.png"
-			#OPTIMIZE Protect against bad strings from Configuration.get() by shell-escaping the arguments.
+		system "gs -q -r#{thumbnail_resolution} -g#{thumbnail_size} -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pngmono -sOutputFile=\"#{thumbnail_file}\" -- \"#{input_file}\""
+		
+		if (File.exist?(raw_file) && ! File.exist?(thumbnail_file))
+			system "convert -resize #{thumbnail_size}\! \"#{raw_file}\" \"#{thumbnail_file}\""
 		end
 		
 		#check if all fallbacks failed and the files are still not present
-		if (! File.exist?(output_path+'.tif'))
+		if (! File.exist?(raw_file))
 			errors.add( :base, I18n.t(:fax_document_not_created))
 		end
-		if (! File.exist?(output_path+'.png'))
+		if (! File.exist?(thumbnail_file))
 			errors.add( :base, I18n.t(:fax_thumbnail_not_created))
 		end
 		
-		self.raw_file = output_file
+		self.raw_file = file_base_name
 		self.file = original_filename
 		return true
 	end
