58cb7cf520269d2dc1474d1b10536ca8b8eac6cb
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-08-15 17:46:16 +0200

    ws

--- app/models/network_setting.rb
+++ app/models/network_setting.rb
@@ -1,31 +1,32 @@
 class NetworkSetting < ActiveRecord::Base
-
-  before_validation(:on => :create) {
+  
+  before_validation( :on => :create ) {
     if NetworkSetting.count > 0
-      errors.add(:network_setting, I18n.t(:allready_configured))
+      errors.add( :network_setting, I18n.t(:allready_configured))
     end
   }
-
-  before_validation(:on => :update) {
+  
+  before_validation( :on => :update ) {
     if ip_address != ip_address_was 
-      errors.add(:ip_address, I18n.t(:can_not_be_changed))
+      errors.add( :ip_address, I18n.t(:can_not_be_changed))
     end
-    if  dhcp_client != dhcp_client_was
-      errors.add(:dhcp_client, I18n.t(:can_not_be_changed))
+    if dhcp_client != dhcp_client_was
+      errors.add( :dhcp_client, I18n.t(:can_not_be_changed))
     end
   }
   
   #validate :validate_settings
- 
-  validates_inclusion_of :interface, :in => ['eth0']
-
-
+  
+  validates_inclusion_of :interface, :in => ['eth0']  #OPTIMIZE Interface name is system-specific.
+  
+  
   network_interfaces = "
   auto lo
   iface lo inet loopback
   "
+  
   after_save {
-    if Configuration.get(:is_appliance, false, Configuration::Boolean)
+    if Configuration.get( :is_appliance, false, Configuration::Boolean )
       if self.dhcp_client == false
         network_interfaces = "
         #{network_interfaces}
@@ -36,7 +37,6 @@ class NetworkSetting < ActiveRecord::Base
         netmask #{netmask}
         #{"gateway #{gateway}" if ! gateway.blank?}    
         "
-        
       else
         network_interfaces = "
         #{network_interfaces}
@@ -44,12 +44,14 @@ class NetworkSetting < ActiveRecord::Base
         iface #{interface} inet dhcp
         "
       end
-      if  ! name_server.empty? && ! dhcp_client
+      
+      if ! name_server.empty? && ! dhcp_client
         resolv_conf= "nameserver #{name_server}"
         write_files('/tmp/resolv', resolv_conf)
       else
         resolv_conf = ""
       end
+      
       if start_dhcp_server
         dnsmasq_conf ="
           dhcp-range=#{dhcp_range_start},#{dhcp_range_end},12h
@@ -59,44 +61,48 @@ class NetworkSetting < ActiveRecord::Base
       else
         dnsmasq_conf = ""
       end
-      file_path_etc = "/tmp/"
+      
+      #OPTIMIZE File paths are system-specific. E.g. limit this to Debian.
+      file_path_etc     = "/tmp/"
       file_path_network = "/tmp/"
       if RAILS_ENV == "production"
-        file_path_etc = "/etc/"
+        file_path_etc     = "/etc/"
         file_path_network = "/etc/network/"
       end
+      
       write_files("#{file_path_etc}dnsmasq.conf", dnsmasq_conf)
       write_files("#{file_path_etc}resolv.conf", resolv_conf)
       write_files("#{file_path_network}interfaces", network_interfaces)
     end
   }
- after_save(:on => :create) {
-  if ! dhcp_client
-    servers = [ 'SipServer', 'SipProxy', 'VoicemailServer']
-    servers.each do |server|
-       if server.constantize.where(:is_local => true).empty?
-        server.constantize.create(:host => ip_address, :is_local => true)
-       end
+  
+  after_save(:on => :create) {
+    if ! dhcp_client
+      servers = [ 'SipServer', 'SipProxy', 'VoicemailServer' ]
+      servers.each do |server|
+        if server.constantize.where(:is_local => true).empty?
+          server.constantize.create(:host => ip_address, :is_local => true)
+        end
+      end
     end
-  end
- }
- 
+  }
+  
   def validate_settings
     if dhcp_client 
       if ! ip_address.empty?
-        errors.add(:ip_address, I18n.t(:must_not_be_set))
+        errors.add( :ip_address, I18n.t(:must_not_be_set))
       end
       if ! netmask.empty?
-        errors.add(:netmask, I18n.t(:must_not_be_set))
+        errors.add( :netmask, I18n.t(:must_not_be_set))
       end
       if ! gateway.empty?
-        errors.add(:gateway, I18n.t(:must_not_be_set))
+        errors.add( :gateway, I18n.t(:must_not_be_set))
       end
       if start_dhcp_server
-        errors.add(:start_dhcp_server, I18n.t(:must_not_be_set))
+        errors.add( :start_dhcp_server, I18n.t(:must_not_be_set))
       end
       if ! name_server.empty?
-        errors.add(:name_server, I18n.t(:must_not_be_set))
+        errors.add( :name_server, I18n.t(:must_not_be_set))
       end
     else
       ActiveRecord::Base.validate_hostname_or_ip :ip_address
@@ -113,8 +119,8 @@ class NetworkSetting < ActiveRecord::Base
   
   private
   
-  def write_files(filename, output)
+  def write_files( filename, output )
     File.open(filename, 'w') {|f| f.write(output) }
   end
- 
+  
 end
