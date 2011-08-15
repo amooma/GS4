61aa4125b72ad9faa80168dabc7b10742a3f200b
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-08-15 18:53:28 +0200

    ws

--- app/controllers/admin/setup_controller.rb
+++ app/controllers/admin/setup_controller.rb
@@ -5,6 +5,7 @@ class Admin::SetupController < ApplicationController
   # https://github.com/ryanb/cancan/wiki/authorizing-controller-actions
   authorize_resource :class => :Setup
   
+  
   def index
     #@User = User.accessible_by( current_ability, :index ).count
   end
@@ -14,12 +15,12 @@ class Admin::SetupController < ApplicationController
       if User.count == 0      
         @user = User.new(params[:user])
         if @user.save
-          format.html { redirect_to(admin_index_path, :notice => t(:user_name_created_login, :user_name => @user.username )) }
+          format.html { redirect_to( admin_index_path, :notice => t(:user_name_created_login, :user_name => @user.username )) }
         else
           format.html { render :action => 'new' }
         end
       else
-        format.html { redirect_to(admin_index_path, :notice => t(:setup_disabled_done)) }
+        format.html { redirect_to( admin_index_path, :notice => t(:setup_disabled_done)) }
       end
     end
   end
