9737b40567733fa8cd6962e284695edf3fb188db
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-08-15 15:30:20 +0200

    ws

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,7 @@ module ApplicationHelper
 		menu_items = []
 		if user_signed_in?
 			case current_user.role
+				
 				when "admin"
 					menu_items = [
 						{ :text => t(:admin)         , :url => admin_index_path },
@@ -14,8 +15,9 @@ module ApplicationHelper
 							{ :text => t(:users)         , :url => admin_users_path },
 							{ :text => t(:sip_accounts)  , :url => sip_accounts_path },
 							{ :text => t(:callforwards)  , :url => call_forwards_path },
-                            { :text => t(:personal_contacts), :url => personal_contacts_path},
-                            { :text => t(:global_contacts), :url => global_contacts_path},						]},
+							{ :text => t(:personal_contacts) , :url => personal_contacts_path },
+							{ :text => t(:global_contacts)   , :url => global_contacts_path },
+						]},
 						
 						{ :text => t(:phones)        , :url => phones_path },
 						
@@ -25,7 +27,7 @@ module ApplicationHelper
 							{ :text => t(:queues)        , :url => call_queues_path },
 							{ :text => t(:conferences)   , :url => conferences_path },
 							{ :text => t(:fax_documents) , :url => fax_documents_path },
-							{ :text => t(:voicemails), :url => voicemails_path},
+							{ :text => t(:voicemails)    , :url => voicemails_path },
 						]},
 						
 						{ :text => t(:servers), :sub => [
@@ -37,20 +39,21 @@ module ApplicationHelper
 						
 						{ :text => t(:help)          , :url => admin_help_path },
 					]
+				
 				when "cdr"
 					menu_items = [
 						{ :text => t(:call_logs)     , :url => call_logs_path },
 					]
+				
 				when "user"
 					menu_items = [
 						{ :text => t(:callforwards)  , :url => call_forwards_path },
 						{ :text => t(:call_logs)     , :url => call_logs_path },
-						{ :text => t(:personal_contacts), :url => personal_contacts_path},
-						{ :text => t(:global_contacts), :url => global_contacts_path},
-						{ :text => t(:voicemails), :url => voicemails_path},
-						{ :text => t(:voicemail_pin_change), :url => pin_change_path},
+						{ :text => t(:personal_contacts) , :url => personal_contacts_path },
+						{ :text => t(:global_contacts)   , :url => global_contacts_path },
+						{ :text => t(:voicemails)    , :url => voicemails_path },
+						{ :text => t(:voicemail_pin_change), :url => pin_change_path },
 						{ :text => t(:conferences)   , :url => conferences_path },
-						
 					]
 			end
 		else
