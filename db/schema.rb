141ffca0b9c132e1b7032b4663c34fa978ec617a
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-09-08 13:15:13 +0200

    bundle exec rake db:migrate !!!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110802131449) do
+ActiveRecord::Schema.define(:version => 20110907075204) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(:version => 20110802131449) do
     t.datetime "received"
     t.datetime "sent"
     t.boolean  "outgoing"
+    t.integer  "user_id"
   end
 
   create_table "global_contacts", :force => true do |t|
