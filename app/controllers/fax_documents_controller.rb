50d9d6cd9d51c935d2b6ded54efbeb5aed243e14
Peter Kozak <peter.kozak@amooma.de>
2011-09-08 13:51:25 +0200

    local access to fax_documents only on create action

--- app/controllers/fax_documents_controller.rb
+++ app/controllers/fax_documents_controller.rb
@@ -2,8 +2,8 @@ class FaxDocumentsController < ApplicationController
   
   before_filter :authenticate_user!, :unless => Proc.new { |r| r.request.remote_ip   == '127.0.0.1' }
   
-  skip_authorization_check
-  
+  load_and_authorize_resource :except => [:create]
+  skip_authorization_check :only => [:create]
   
   before_filter { |controller|
     @users = User.accessible_by( current_ability, :index ).all
