ed4c6635ccf2850e62463b697610ca7a6355c1f6
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-09-11 23:53:22 +0200

    ws gefixt

--- app/controllers/admin/wizard_phone_and_user_controller.rb
+++ app/controllers/admin/wizard_phone_and_user_controller.rb
@@ -1,26 +1,28 @@
 class Admin::WizardPhoneAndUserController < ApplicationController
-before_filter :authenticate_user!
-
-skip_authorization_check
-
-before_filter{
+  
+  before_filter :authenticate_user!
+  
+  ## https://github.com/ryanb/cancan/wiki/authorizing-controller-actions
+  #load_and_authorize_resource
+  skip_authorization_check
+  
+  before_filter{
     @sip_proxy = SipProxy.where(:is_local => true).first
     @sip_server = SipServer.where(:is_local => true).first
     @voicemail_server = VoicemailServer.where(:is_local => true).first
     @auth_name = SecureRandom.hex(10) if ! @auth_name
     @sip_password = SecureRandom.hex(15) if ! @sip_password
     @phone_models = PhoneModel.all
-
-}
+  }
+  
   def new
-      
+    
     respond_to do |format|
       format.html 
     end
   end
+  
   def create
-
-    
     @user = User.find_or_create_by_username(
       :username => params[:wusername],
       :first_name => params[:wfirst_name],
@@ -33,13 +35,16 @@ before_filter{
     if @user.id.nil?
       @user.save
     end
+    
     @phone = Phone.find_or_create_by_mac_address(
       :mac_address => params[:wmac_address].upcase,
       :phone_model_id => params[:phone_model_id]
     )
+    
     if @phone.id.nil?
       @phone.save
     end
+    
     if ! @phone.id.nil?
       @sip_account = SipAccount.find_or_create_by_caller_name_and_user_id(
         :user_id => @user.id,
@@ -52,23 +57,21 @@ before_filter{
         :voicemail_pin => params[:wvoicemail_pin],
         :caller_name => params[:wcaller_name],
         :phone_id => @phone.id
-        
       )
-      
       if @sip_account.id.nil?
         @sip_account.save
       end
     end
+    
     @extension = Extension.find_or_create_by_extension_and_destination(
       :extension => params[:wextension],
       :destination => @auth_name,
       :active => true
-
-      
     )
     if @extension.id.nil?
       @extension.save
     end
+    
     if ! @extension.id.nil? && ! @sip_account.id.nil?
       @sip_account_to_extension = SipAccountToExtension.find_or_create_by_extension_id_and_sip_account_id(
         :extension_id => @extension.id,
@@ -78,12 +81,14 @@ before_filter{
         @extension.save
       end
     end
+    
     respond_to do |format|
-      if  ! @user.id.nil? && ! @phone.id.nil? && ! @sip_account.id.nil? && ! @extension.id.nil?
+      if ! @user.id.nil? && ! @phone.id.nil? && ! @sip_account.id.nil? && ! @extension.id.nil?
         format.html { redirect_to( admin_index_path, :notice => I18n.t(:wizard_phone_and_user_saved) ) }
-        else
+      else
         format.html { render :action => "new" }
       end 
     end
   end
+  
 end
