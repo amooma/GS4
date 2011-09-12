a9a5d7af8dea323bb87c41b927f42953521adaf8
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-09-12 02:08:24 +0200

    ws

--- app/models/reboot_request.rb
+++ app/models/reboot_request.rb
@@ -1,7 +1,10 @@
 class RebootRequest < ActiveRecord::Base
-validates_presence_of :phone_id
+  
+  validates_presence_of :phone_id
   validates_numericality_of :phone_id, :only_integer => true, :greater_than_or_equal_to => 0
   belongs_to :phone
+  
   default_value_for :start, Time.now
   default_value_for :successful, false
+  
 end
