bfc3d74adc7b3380d74fcd03aff947653ce57ae4
Philipp Kempgen <philipp.kempgen@amooma.de>
2011-09-12 02:07:50 +0200

    ws

--- app/models/phone_model_key.rb
+++ app/models/phone_model_key.rb
@@ -1,4 +1,5 @@
 class PhoneModelKey < ActiveRecord::Base
+  
   belongs_to :phone_model, :validate => true
   has_many :phone_key_to_function_mappings
   has_many :phone_key_function_definitions, :through => :phone_key_to_function_mappings
@@ -6,6 +7,7 @@ class PhoneModelKey < ActiveRecord::Base
   
   validates_presence_of     :phone_model_id
   validates_numericality_of :phone_model_id, :only_integer => true
-
+  
   validates_uniqueness_of :name, :scope => :phone_model_id
+  
 end
