cd1418b325ea02e1aa0f027f610130d27a97c8bb
Philipp Kempgen <lists@kempgen.net>
2011-09-16 02:07:36 +0200

    ws

--- app/mailers/fax_mailer.rb
+++ app/mailers/fax_mailer.rb
@@ -1,22 +1,27 @@
+#TODO Add this file in the Debian package.
+
 class FaxMailer < ActionMailer::Base
-	default :from => Configuration.get(:smarthost_from_address, 'Gemeinschaft4')
+	default :from => Configuration.get( :smarthost_from_address, 'Gemeinschaft4' )
 	
-	def new_fax_document(fax_document)
-		@user = fax_document.user
-        @fax_document = fax_document
+	def new_fax_document( fax_document )
+		@user         = fax_document.user
+		@fax_document = fax_document
 		
 		if @user
-			file_path = @fax_document.pdf_file_path
-			if (! file_path)
-				file_path =  @fax_document.to_pdf
-				
-			end
+			file_path   = @fax_document.pdf_file_path
+			file_path ||= @fax_document.to_pdf
+			
 			if (file_path)
-				file_name = File.basename(@fax_document.file, File.extname(@fax_document.file)) + '.pdf'
+				file_name = File.basename( @fax_document.file, File.extname(@fax_document.file) ) + '.pdf'
+			else
+				file_name = nil
 			end
 			
-			attachments[file_name] = File.read(file_path)
-			mail(:to => "#{@user.gn} #{@user.sn} <#{@user.email}>", :subject => t(:fax_email_subject, :source => @fax_document.source, :destination => @fax_document.destination))
+			attachments[ file_name ] = File.read( file_path )
+			mail(
+				:to => "#{@user.gn} #{@user.sn} <#{@user.email}>",
+				:subject => t(:fax_email_subject, :source => @fax_document.source, :destination => @fax_document.destination )
+			)
 		end
 	end
 end
