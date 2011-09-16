4faf20954e3fff47b3505529efef94f9f1acfbc5
Philipp Kempgen <lists@kempgen.net>
2011-09-16 22:16:32 +0200

    typo und ws

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -49,11 +49,13 @@ class Extension < ActiveRecord::Base
   
   after_save :update_default_extension_length_if_needed
   
-  # Change the default lenght of an extension when is seems to need changed.
+  # Change the default length of an extension when is looks like it
+  # needs to be changed.
   # It assumes that a new length automaticly results in a new default.
   #
   def update_default_extension_length_if_needed
-    if self.extension.to_i.to_s == self.extension.to_s && self.extension.to_s.length != Configuration.get(:default_extension_length).to_i
+    if self.extension.to_i.to_s == self.extension.to_s \
+    && self.extension.to_s.length != Configuration.get(:default_extension_length).to_i
       Configuration.where(:name => 'default_extension_length').first.update_attributes(:value => self.extension.to_s.length)
     end
   end
@@ -63,12 +65,14 @@ class Extension < ActiveRecord::Base
   def self.next_unused_extension
     number_of_digits = Configuration.get(:default_extension_length, 3, Integer)
     check_extension = 10 ** (number_of_digits - 1)
-    if Extension.last != nil && 
-       Extension.last.extension.to_i.to_s == Extension.last.extension.to_s && 
-       Extension.last.extension.to_i + 1 < (10 ** number_of_digits) - 1 && 
-       (Extension.last.extension.to_i + 1).to_s.length == number_of_digits
+    
+    if  Extension.last \
+    &&  Extension.last.extension.to_i.to_s == Extension.last.extension.to_s \
+    &&  Extension.last.extension.to_i + 1 < (10 ** number_of_digits) - 1 \
+    && (Extension.last.extension.to_i + 1).to_s.length == number_of_digits
       check_extension = Extension.last.extension.to_i + 1
     end
+    
     while Extension.exists?( :extension => check_extension.to_s )
       check_extension += 1
     end
