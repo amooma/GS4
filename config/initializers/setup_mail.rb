76ebf4f13304177eff550942411cc58b9f1a8748
Philipp Kempgen <lists@kempgen.net>
2011-09-16 17:57:33 +0200

    SQLite3::SQLException: no such table: configurations: SELECT  "configurations".* FROM "configurations" WHERE "configurations"."name" = 'smarthost_hostname' LIMIT 1

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,37 @@
-ActionMailer::Base.smtp_settings = {
-	:address               => Configuration.get( :smarthost_hostname, '127.0.0.1' ),
-	:port                  => Configuration.get( :smarthost_port, 25, Integer ),
-	:domain                => Configuration.get( :smarthost_domain, 'gemeinschaft.local' ),
-	:user_name             => Configuration.get( :smarthost_username, '' ),
-	:password              => Configuration.get( :smarthost_password, '' ),
-	:authentication        => Configuration.get( :smarthost_authentication, 'plain' ),
-	:enable_starttls_auto  => Configuration.get( :smarthost_enable_starttls_auto, true, Configuration::Boolean ),
-}
+begin
+	settings = {
+		:address               => Configuration.get( :smarthost_hostname, '127.0.0.1' ),
+		:port                  => Configuration.get( :smarthost_port, 25, Integer ),
+		:domain                => Configuration.get( :smarthost_domain, 'gemeinschaft.local' ),
+		:user_name             => Configuration.get( :smarthost_username, '' ),
+		:password              => Configuration.get( :smarthost_password, '' ),
+		:authentication        => Configuration.get( :smarthost_authentication, 'plain' ),
+		:enable_starttls_auto  => Configuration.get( :smarthost_enable_starttls_auto, true, Configuration::Boolean ),
+	}
+
+rescue ActiveRecord::StatementInvalid => e
+	if ($0.to_s.match( /\b(rake)\b/ ) \
+	&&  ::Object.const_defined?( :Rake ) \
+	)
+		#STDERR.puts "------#{Rake.application.top_level_tasks}"
+		#STDERR.puts "------#{Rake.application.lookup( Rake.application.top_level_tasks.first ).prerequisites}"
+		if [
+			'db:schema:load',
+			'db:setup',
+			'db:migrate',
+			'db:create',
+		].include?( (Rake.application.top_level_tasks || []).first )
+			
+			STDERR.puts [
+				"Could not get configuration from the database.",
+				"Looks like you're just setting up the database,",
+				"so no need to worry.",
+			]
+		end
+	else
+		raise e
+	end
+end
+
+ActionMailer::Base.smtp_settings = settings || {}
+
