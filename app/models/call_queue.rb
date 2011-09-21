1e35c289857abd5a8ba7fc77c3f3ccb6e5b35b69
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2011-09-21 18:49:49 +0200

    Workflow fuer Queues verbessert.

--- app/models/call_queue.rb
+++ app/models/call_queue.rb
@@ -2,6 +2,8 @@ class CallQueue < ActiveRecord::Base
   has_many :call_queue_to_extensions, :dependent => :destroy
   has_many :extensions, :through => :call_queue_to_extensions
   
+  accepts_nested_attributes_for :extensions
+  
   validates_uniqueness_of :uuid
   validates_presence_of :uuid
   
