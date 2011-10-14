bf65e223ab9d9b1ed08db45c2aee2f617740c9d4
Sascha Daniels <sd@alternative-solution.de>
2011-10-14 16:41:40 +0200

    Storing Session in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111013104213) do
+ActiveRecord::Schema.define(:version => 20111014142628) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -370,6 +370,16 @@ ActiveRecord::Schema.define(:version => 20111013104213) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sip_account_codecs", :force => true do |t|
     t.integer  "codec_id"
