71e495ac873741fbf4f8b52dcdcdcfda6cdd3e92
Sascha Daniels <sd@alternative-solution.de>
2012-01-09 16:44:51 +0100

    New Apparmor Profiles

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,10 @@
 class Location < ActiveRecord::Base
   set_table_name "location"
+before_validation {
+    raise ActiveRecord::ReadOnlyRecord
+}
+
+def before_destroy
+    raise ActiveRecord::ReadOnlyRecord
+end
 end
