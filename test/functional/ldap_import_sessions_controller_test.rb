00dbb93880d5c5432d2c18649a943f5aa390131c
Sascha Daniels <sd@alternative-solution.de>
2012-01-18 11:43:32 +0100

    Function not active at the moment

--- test/functional/ldap_import_sessions_controller_test.rb
+++ test/functional/ldap_import_sessions_controller_test.rb
@@ -1,49 +1,49 @@
 require 'test_helper'
 
 class LdapImportSessionsControllerTest < ActionController::TestCase
-  setup do
-    @ldap_import_session = ldap_import_sessions(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:ldap_import_sessions)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create ldap_import_session" do
-    assert_difference('LdapImportSession.count') do
-      post :create, :ldap_import_session => @ldap_import_session.attributes
-    end
-
-    assert_redirected_to ldap_import_session_path(assigns(:ldap_import_session))
-  end
-
-  test "should show ldap_import_session" do
-    get :show, :id => @ldap_import_session.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @ldap_import_session.to_param
-    assert_response :success
-  end
-
-  test "should update ldap_import_session" do
-    put :update, :id => @ldap_import_session.to_param, :ldap_import_session => @ldap_import_session.attributes
-    assert_redirected_to ldap_import_session_path(assigns(:ldap_import_session))
-  end
-
-  test "should destroy ldap_import_session" do
-    assert_difference('LdapImportSession.count', -1) do
-      delete :destroy, :id => @ldap_import_session.to_param
-    end
-
-    assert_redirected_to ldap_import_sessions_path
-  end
+  #setup do
+  #  @ldap_import_session = ldap_import_sessions(:one)
+  #end
+  #
+  #test "should get index" do
+  #  get :index
+  #  assert_response :success
+  #  assert_not_nil assigns(:ldap_import_sessions)
+  #end
+  #
+  #test "should get new" do
+  #  get :new
+  #  assert_response :success
+  #end
+  #
+  #test "should create ldap_import_session" do
+  #  assert_difference('LdapImportSession.count') do
+  #    post :create, :ldap_import_session => @ldap_import_session.attributes
+  #  end
+  #
+  #  assert_redirected_to ldap_import_session_path(assigns(:ldap_import_session))
+  #end
+  #
+  #test "should show ldap_import_session" do
+  #  get :show, :id => @ldap_import_session.to_param
+  #  assert_response :success
+  #end
+  #
+  #test "should get edit" do
+  #  get :edit, :id => @ldap_import_session.to_param
+  #  assert_response :success
+  #end
+  #
+  #test "should update ldap_import_session" do
+  #  put :update, :id => @ldap_import_session.to_param, :ldap_import_session => @ldap_import_session.attributes
+  #  assert_redirected_to ldap_import_session_path(assigns(:ldap_import_session))
+  #end
+  #
+  #test "should destroy ldap_import_session" do
+  #  assert_difference('LdapImportSession.count', -1) do
+  #    delete :destroy, :id => @ldap_import_session.to_param
+  #  end
+  #
+  #  assert_redirected_to ldap_import_sessions_path
+  #end
 end
